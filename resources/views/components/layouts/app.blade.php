bd2b02514c766ee9e5faa32029eaadf87bc505af
HTAJnr <helderjuniorvucas13@gmail.com>
2024-10-11 17:53:28 +0200

    Criação do Proposal

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Laravel App</title>
+</head>
+<body>
+    <header>
+        <!-- <h1>My App Header</h1> -->
+    </header>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    <footer>
+        <!-- <p>My App Footer</p> -->
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::view('/', 'projects.index')->name('projects.index');
+
